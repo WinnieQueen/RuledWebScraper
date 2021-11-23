5094935d538d8d679cc8fc0c06cc6dfec02a947a
WinnieQueen <ashilliard@student.neumont.edu>
2021-11-23 06:22:40 -0700

    TABLE WORKS! ATTRIBUTES WORK! ALL TAGS SHOW! YAY!

--- RuledWebScraper/Shared/Models/TestedTag.cs
+++ RuledWebScraper/Shared/Models/TestedTag.cs
@@ -1,25 +1,42 @@
 ﻿using System.Collections.Generic;
 
 namespace RuledWebScraper.Shared.Models {
-  public class TestedTag {
-    private string tag = "";
-    private List<string> missingAttributes;
-    private bool passed;
+   public class TestedTag {
+      private string tag = "";
+      private List<string> missingAttributes;
+      private bool passed;
 
-    public TestedTag(string tag, List<string> missingAttributes, bool passed) {
-      this.tag = tag;
-      this.missingAttributes = missingAttributes;
-      this.passed = passed;
-    }
+      public TestedTag(string tag, List<string> missingAttributes, bool passed)
+      {
+         this.tag = tag;
+         this.missingAttributes = missingAttributes;
+         this.passed = passed;
+      }
 
-    public string GetTag() {
-      return tag;
-    }
-    public string[] GetMissingAttributes() {
-      return missingAttributes.ToArray();
-    }
-    public bool IsPassing() {
-      return passed;
-    }
-  }
+      public string GetTag()
+      {
+         return tag;
+      }
+      public string[] GetMissingAttributes()
+      {
+         return missingAttributes.ToArray();
+      }
+      public string GetMissingAttributesAsString()
+      {
+         string allAtts = "";
+         if (missingAttributes.Count > 0)
+         {
+            allAtts = missingAttributes[0];
+            foreach (string attribute in missingAttributes)
+            {
+               allAtts += ", " + attribute;
+            }
+         }
+         return allAtts;
+      }
+      public bool IsPassing()
+      {
+         return passed;
+      }
+   }
 }
--- RuledWebScraper/Shared/Models/WebScraper.cs
+++ RuledWebScraper/Shared/Models/WebScraper.cs
@@ -4,98 +4,139 @@ using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace RuledWebScraper.Shared.Models {
-  public class WebScraper {
-    private List<string> required;
-    private string url;
-    public WebScraper(string url, params string[] required) {
-      SetRequired(required);
-      this.url = url;
-    }
-
-    public async Task<string> GetHtmlFromPage() {
-      string html = null;
-      try {
-        HttpClient client = new();
-
-        html = await client.GetStringAsync(GetUrl());
-      } catch (Exception e) {
-        Console.WriteLine("Hit exception " + e.Message);
-      }
-      return html;
-    }
-
-    public Dictionary<int, TestedTag> TestAllTags(string html, params string[] attributes) {
-      Dictionary<int, TestedTag> results = new Dictionary<int, TestedTag>();
-      List<string> allTags = GetAllTags(html);
-      int numOfTags = allTags.Count;
-      Console.WriteLine(html);
-
-      for (int tagIndex = 0; tagIndex < numOfTags; tagIndex++) {
-        bool tagContainedAttributes = true;
-        List<string> missingAttributes = new List<string>();
-
-        string currentTag = allTags[tagIndex];
-        for (int attributeIndex = 0; attributeIndex < attributes.Length; attributeIndex++) {
-
-          string currentAttribute = attributes[attributeIndex];
-          if (!currentTag.Contains(currentAttribute)) {
-            tagContainedAttributes = false;
-            missingAttributes.Add(currentAttribute);
-          }
-        }
-
-        TestedTag testedTag = new TestedTag(currentTag, missingAttributes, tagContainedAttributes);
-        results.Add(tagIndex, testedTag);
+   public class WebScraper {
+      private List<string> required;
+      private string url;
+      public WebScraper(string url, params string[] required)
+      {
+         SetRequired(required);
+         this.url = url;
       }
 
-      return results;
-    }
+      public async Task<string> GetHtmlFromPage()
+      {
+         string html = null;
+         try
+         {
+            HttpClient client = new();
 
-    public List<string> GetAllTags(string html) {
-      List<string> allTags = new List<string>();
-      string pattern = @"<((\w*)( \w*=(""|')\w*(""|'))*)> ";
-      var matches = Regex.Matches(html, pattern);
-
-      for (int i = 0; i < matches.Count; i++) {
-
-        allTags.Add(matches[i].ToString());
-        Console.WriteLine(allTags[i]);
-
+            html = await client.GetStringAsync(GetUrl());
+         }
+         catch (Exception e)
+         {
+            Console.WriteLine("Hit exception " + e.Message);
+         }
+         return html;
       }
-      return allTags;
-    }
 
-    public string GetUrl() {
-      return url;
-    }
+      public Dictionary<int, TestedTag> TestAllTags(string html, params string[] attributes)
+      {
+         Dictionary<int, TestedTag> results = new Dictionary<int, TestedTag>();
+         List<string> allTags = GetAllTags(html);
+         int numOfTags = allTags.Count;
 
-    public void SetUrl(string url) {
-      if (!Uri.IsWellFormedUriString(url, UriKind.Absolute)) {
-        throw new ArgumentException("Must pass in a valid URL");
+         for (int tagIndex = 0; tagIndex < numOfTags; tagIndex++)
+         {
+            bool tagContainedAttributes = true;
+            List<string> missingAttributes = new List<string>();
+
+            string currentTag = allTags[tagIndex];
+
+            Console.WriteLine(tagIndex + ": " + currentTag);
+            for (int attributeIndex = 0; attributeIndex < attributes.Length; attributeIndex++)
+            {
+
+               string currentAttribute = attributes[attributeIndex];
+               if (!currentTag.Contains(currentAttribute))
+               {
+                  tagContainedAttributes = false;
+                  missingAttributes.Add(currentAttribute);
+               }
+            }
+
+            TestedTag testedTag = new TestedTag(currentTag, missingAttributes, tagContainedAttributes);
+            results.Add(tagIndex, testedTag);
+         }
+
+         return results;
       }
-      this.url = url;
-    }
 
-    #region Alter Rules
+      public List<string> GetAllTags(string html)
+      {
+         List<string> allTags = new List<string>();
+         HtmlDocument doc = new HtmlDocument();
+         doc.LoadHtml(html);
+         IEnumerable<HtmlNode> coll = doc.DocumentNode.Descendants(HtmlDocument.MaxDepthLevel);
 
-    public List<string> GetRequired() {
-      return new List<string>(this.required.ToArray());
-    }
+         List<string> nodeList = new List<string>();
+         LevelOneNodes(coll, ref nodeList);
 
-    public void SetRequired(params string[] required) {
-      this.required = new List<string>(required.ToArray());
-    }
+         foreach (string node in nodeList)
+         {
+            allTags.Add(node);
+            Console.WriteLine("Node: " + node);
+         }
 
-    public void AddRules(params string[] required) {
-      this.required.AddRange(required);
-    }
+         return allTags;
+      }
 
-    public void RemoveRules(params string[] toRemove) {
-      this.required.RemoveAll(x => toRemove.Contains(x));
-    }
+      private void LevelOneNodes(IEnumerable<HtmlNode> nodeList, ref List<string> finalList)
+      {
+         foreach (HtmlNode node in nodeList)
+         {
+            if (node.OuterHtml.StartsWith("<") && !node.OuterHtml.StartsWith("<!--"))
+            {
+               if (node.HasChildNodes)
+               {
+                  LevelOneNodes(node.ChildNodes, ref finalList);
+               }
+               else
+               {
+                  finalList.Add(node.OuterHtml);
+               }
+            }
+         }
+      }
 
-    #endregion
-  }
+      public string GetUrl()
+      {
+         return url;
+      }
+
+      public void SetUrl(string url)
+      {
+         if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+         {
+            throw new ArgumentException("Must pass in a valid URL");
+         }
+         this.url = url;
+      }
+
+      #region Alter Rules
+
+      public List<string> GetRequired()
+      {
+         return new List<string>(this.required.ToArray());
+      }
+
+      public void SetRequired(params string[] required)
+      {
+         this.required = new List<string>(required.ToArray());
+      }
+
+      public void AddRules(params string[] required)
+      {
+         this.required.AddRange(required);
+      }
+
+      public void RemoveRules(params string[] toRemove)
+      {
+         this.required.RemoveAll(x => toRemove.Contains(x));
+      }
+
+      #endregion
+   }
 }
