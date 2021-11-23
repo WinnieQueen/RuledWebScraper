8ef09c4c4e7b4c689b5889ed40876cbadedeb33d
WinnieQueen <ashilliard@student.neumont.edu>
2021-11-23 10:06:50 -0700

    fixed the tests

--- RuledWebScraper/Shared/Models/WebScraper.cs
+++ RuledWebScraper/Shared/Models/WebScraper.cs
@@ -35,7 +35,7 @@ namespace RuledWebScraper.Shared.Models {
       public Dictionary<int, TestedTag> TestAllTags(string html, params string[] attributes)
       {
          Dictionary<int, TestedTag> results = new Dictionary<int, TestedTag>();
-         List<string> allTags = GetAllTags(html);
+         List<HtmlNode> allTags = GetAllTags(html);
          int numOfTags = allTags.Count;
 
          for (int tagIndex = 0; tagIndex < numOfTags; tagIndex++)
@@ -43,83 +43,49 @@ namespace RuledWebScraper.Shared.Models {
             bool tagContainedAttributes = true;
             List<string> missingAttributes = new List<string>();
 
-            string currentTag = allTags[tagIndex];
+            HtmlNode currentTag = allTags[tagIndex];
 
             for (int attributeIndex = 0; attributeIndex < attributes.Length; attributeIndex++)
             {
 
                string currentAttribute = attributes[attributeIndex];
-               if (!currentTag.Contains(currentAttribute))
+               if (!currentTag.Attributes.Contains(currentAttribute))
                {
                   tagContainedAttributes = false;
-                  Console.WriteLine(currentTag + " Added " + currentAttribute);
                   missingAttributes.Add(currentAttribute);
                }
             }
-
-            TestedTag testedTag = new TestedTag(currentTag, missingAttributes, tagContainedAttributes);
+            string tagToString = currentTag.OuterHtml;
+            string inner = currentTag.InnerHtml;
+            if (inner.Length > 0)
+            {
+               tagToString = currentTag.OuterHtml.Replace(inner, " ... ");
+            }
+            TestedTag testedTag = new TestedTag(tagToString, missingAttributes, tagContainedAttributes);
             results.Add(tagIndex, testedTag);
          }
 
          return results;
       }
 
-      public List<string> GetAllTags(string html)
+      public List<HtmlNode> GetAllTags(string html)
       {
-         List<string> allTags = new List<string>();
+         List<HtmlNode> allTags = new List<HtmlNode>();
          HtmlDocument doc = new HtmlDocument();
          doc.LoadHtml(html);
          IEnumerable<HtmlNode> coll = doc.DocumentNode.Descendants(HtmlDocument.MaxDepthLevel);
 
-         //List<string> nodeList = new List<string>();
-         //LevelOneNodes(coll, ref nodeList);
-
-         List<string> nodeList = LevelOneNodes(coll);
-
-         foreach (string node in nodeList)
+         foreach (HtmlNode node in coll)
          {
-            allTags.Add(node);
+            string nodeText = node.OuterHtml;
+            if (nodeText.StartsWith("<") && !nodeText.StartsWith("<!--") && !nodeText.StartsWith("<!doctype") && !nodeText.StartsWith("<meta"))
+            {
+               allTags.Add(node);
+            }
          }
 
          return allTags;
       }
-
-      private List<string> LevelOneNodes(IEnumerable<HtmlNode> nodeList)
-      {
-         List<string> finalList = new List<string>();
-         foreach (HtmlNode node in nodeList)
-         {
-            if (node.OuterHtml.StartsWith("<") && !node.OuterHtml.StartsWith("<!--") && !node.OuterHtml.StartsWith("<!doctype") && !node.OuterHtml.StartsWith("<meta"))
-            {
-               if (node.HasChildNodes)
-               {
-                  finalList.AddRange(LevelOneNodes(node.ChildNodes));
-               }
-               else
-               {
-                  finalList.Add(node.OuterHtml);
-               }
-            }
-         }
-         return finalList;
-      }
-      //private void LevelOneNodes(IEnumerable<HtmlNode> nodeList, ref List<string> finalList)
-      //{
-      //   foreach (HtmlNode node in nodeList)
-      //   {
-      //      if (node.OuterHtml.StartsWith("<") && !node.OuterHtml.StartsWith("<!--") && !node.OuterHtml.StartsWith("<!doctype") && !node.OuterHtml.StartsWith("<meta"))
-      //      {
-      //         if (node.HasChildNodes)
-      //         {
-      //            LevelOneNodes(node.ChildNodes, ref finalList);
-      //         }
-      //         else
-      //         {
-      //            finalList.Add(node.OuterHtml);
-      //         }
-      //      }
-      //   }
-      //}
 
       public string GetUrl()
       {
--- RuledWebScraperTests/HtmlTests.cs
+++ RuledWebScraperTests/HtmlTests.cs
@@ -2,71 +2,179 @@
 using System;
 using System.Collections.Generic;
 using Xunit;
+using HtmlAgilityPack;
 
 namespace RuledWebScraperTests {
-  public class HtmlTests {
-    private string sampleLink = "https://cbracco.github.io/html5-test-page/";
+   public class HtmlTests {
+      private string sampleLink = "https://cbracco.github.io/html5-test-page/";
 
 
-    [Fact]
-    public void CanCreateScraper() {
-      WebScraper scraper = new("google.com");
+      [Fact]
+      public void CanCreateScraper()
+      {
+         WebScraper scraper = new("google.com");
 
-      Assert.NotNull(scraper);
-    }
-
-    [SkippableFact]
-    public void CanAccessWebpage() {
-      WebScraper scraper;
-      try {
-        scraper = new(sampleLink);
-
-      } catch (Exception e) {
-        throw new SkipException("Unable to create WebScraper, cannot test methods", e);
-      }
-      string html = scraper.GetHtmlFromPage().Result;
-      Assert.Contains("<!doctype html>", html.ToLower());
-    }
-
-    [SkippableFact]
-    public void TagsContainAttribute() {
-      WebScraper scraper;
-      try {
-        scraper = new(sampleLink);
-      } catch (Exception e) {
-        throw new SkipException("Unable to create WebScraper, cannot test methods", e);
-      }
-      try {
-        string html = "<p id='myId'> </p>";
-        string attribute = "id";
-        Dictionary<int, TestedTag> results = scraper.TestAllTags(html, attribute);
-        Assert.NotEmpty(results);
-        Assert.True(results[0].IsPassing());
-      } catch (NullReferenceException e) {
-        throw new SkipException("An unexpected exception was thrown, methods cannot be tested." + e.Message, e);
+         Assert.NotNull(scraper);
       }
 
-    }
+      [SkippableFact]
+      public void CanAccessWebpage()
+      {
+         WebScraper scraper;
+         try
+         {
+            scraper = new(sampleLink);
 
-    [SkippableFact]
-    public void TagsDoNotContainAttribute() {
-      WebScraper scraper;
-      try {
-        scraper = new(sampleLink);
-      } catch (Exception e) {
-        throw new SkipException("Unable to create WebScraper, cannot test methods", e);
+         }
+         catch (Exception e)
+         {
+            throw new SkipException("Unable to create WebScraper, cannot test methods", e);
+         }
+         string html = scraper.GetHtmlFromPage().Result;
+         Assert.Contains("<!doctype html>", html.ToLower());
       }
-      try {
-        string html = "<p> </p>";
-        string attribute = "id";
-        Dictionary<int, TestedTag> results = scraper.TestAllTags(html, attribute);
 
-        Assert.NotEmpty(results);
-        Assert.False(results[0].IsPassing());
-      } catch (NullReferenceException e) {
-        throw new SkipException("An unexpected exception was thrown, methods cannot be tested." + e.Message, e);
+      [SkippableFact]
+      public void TagsContainAttribute()
+      {
+         WebScraper scraper;
+         try
+         {
+            scraper = new(sampleLink);
+         }
+         catch (Exception e)
+         {
+            throw new SkipException("Unable to create WebScraper, cannot test methods", e);
+         }
+         try
+         {
+            string html = @"<!DOCTYPE html> <html> <p id='myId'> </p> </html>";
+            string attribute = "id";
+            Dictionary<int, TestedTag> results = scraper.TestAllTags(html, attribute);
+            Assert.NotEmpty(results);
+            Assert.True(results[2].IsPassing());
+            Assert.Empty(results[2].GetMissingAttributes());
+            Assert.Equal("", results[2].GetMissingAttributesAsString());
+         }
+         catch (NullReferenceException e)
+         {
+            throw new SkipException("An unexpected exception was thrown, methods cannot be tested." + e.Message, e);
+         }
+
       }
-    }
 
-  }
+      [SkippableFact]
+      public void TagsContainMultipleAttributes()
+      {
+         WebScraper scraper;
+         try
+         {
+            scraper = new(sampleLink);
+         }
+         catch (Exception e)
+         {
+            throw new SkipException("Unable to create WebScraper, cannot test methods", e);
+         }
+         try
+         {
+            string html = @"<!DOCTYPE html> <html> <p class='MyClass' id='myId'> </p> </html>";
+            Dictionary<int, TestedTag> results = scraper.TestAllTags(html, "id", "class");
+            Assert.NotEmpty(results);
+            Assert.True(results[2].IsPassing());
+            Assert.Empty(results[2].GetMissingAttributes());
+            Assert.Equal("", results[2].GetMissingAttributesAsString());
+         }
+         catch (NullReferenceException e)
+         {
+            throw new SkipException("An unexpected exception was thrown, methods cannot be tested." + e.Message, e);
+         }
+
+      }
+
+      [SkippableFact]
+      public void CanGetTags()
+      {
+         WebScraper scraper;
+         try
+         {
+            scraper = new(sampleLink);
+         }
+         catch (Exception e)
+         {
+            throw new SkipException("Unable to create WebScraper, cannot test methods", e);
+         }
+         try
+         {
+            string html = @"<!DOCTYPE html> <html> <p id='myId'> </p> </html>";
+            string expected = "<p id='myId'> </p>";
+            List<HtmlNode> results = scraper.GetAllTags(html);
+            Assert.NotEmpty(results);
+            Assert.Equal(expected, results[2].OuterHtml);
+         }
+         catch (NullReferenceException e)
+         {
+            throw new SkipException("An unexpected exception was thrown, methods cannot be tested." + e.Message, e);
+         }
+
+      }
+
+      [SkippableFact]
+      public void TagsDoNotContainAttribute()
+      {
+         WebScraper scraper;
+         try
+         {
+            scraper = new(sampleLink);
+         }
+         catch (Exception e)
+         {
+            throw new SkipException("Unable to create WebScraper, cannot test methods", e);
+         }
+         try
+         {
+            string html = @"<!DOCTYPE html> <html> <p class='MyClass'> </p> </html>";
+            string attribute = "id";
+            Dictionary<int, TestedTag> results = scraper.TestAllTags(html, attribute);
+
+            Assert.NotEmpty(results);
+            Assert.False(results[2].IsPassing());
+            Assert.Equal("id", results[2].GetMissingAttributes()[0]);
+            Assert.Equal("id", results[2].GetMissingAttributesAsString());
+         }
+         catch (NullReferenceException e)
+         {
+            throw new SkipException("An unexpected exception was thrown, methods cannot be tested." + e.Message, e);
+         }
+      }
+
+      [SkippableFact]
+      public void TagsContainOneOfTwoAttributes()
+      {
+         WebScraper scraper;
+         try
+         {
+            scraper = new(sampleLink);
+         }
+         catch (Exception e)
+         {
+            throw new SkipException("Unable to create WebScraper, cannot test methods", e);
+         }
+         try
+         {
+            string html = @"<!DOCTYPE html> <html> <p id='myId'> </p> </html>";
+            Dictionary<int, TestedTag> results = scraper.TestAllTags(html, "id", "class");
+            Assert.NotEmpty(results);
+            Assert.False(results[2].IsPassing());
+            Assert.NotEmpty(results[2].GetMissingAttributes());
+            Assert.Equal("class", results[2].GetMissingAttributes()[0]);
+            Assert.Equal("class", results[2].GetMissingAttributesAsString());
+         }
+         catch (NullReferenceException e)
+         {
+            throw new SkipException("An unexpected exception was thrown, methods cannot be tested." + e.Message, e);
+         }
+
+      }
+
+   }
 }
